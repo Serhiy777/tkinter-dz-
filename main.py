da2dd90aa19d92967df5ae891839fc645641ff8e
Serhiy <97907231+Serhiy777@users.noreply.github.com>
2022-12-24 19:00:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 from tkinter import *
 
 def mph_to_km():
@@ -15,19 +16,19 @@ def km_to_mph():
 
 window = Tk()
 window.geometry('411x206+100+100')
-window.title('MI to KM | Converter')
+window.title('MI \N{RIGHTWARDS BLACK ARROW} KM | Converter')
 window['bg'] = "#000000"
 
-label1 = Label(text='MI to KM', bg='black', fg='white')
+label1 = Label(text='MI \N{RIGHTWARDS BLACK ARROW} KM', bg='black', fg='white', font='Tahoma')
 label1.pack(pady=10)
 
-label2 = Label(text='Input MI:', bg='black', fg='white')
+label2 = Label(text='Input MI:', bg='black', fg='white', font='Tahoma')
 label2.place(x=10 ,y=70)
 
-label3 = Label(text='Input KM:', bg='black', fg='white')
+label3 = Label(text='Input KM:', bg='black', fg='white', font='Tahoma')
 label3.place(x=10 ,y=110)
 
-label4 = Label(text='Result:', bg='black', fg='white')
+label4 = Label(text='Result:', bg='black', fg='white', font='Tahoma')
 label4.place(x=10, y=160)
 
 entry2 = Entry()
@@ -42,11 +43,10 @@ entry4 = Entry()
 entry4.place(x=100, y=160)
 entry4.insert(0,'0')
 
-btn1 = Button(text='MPH \N{RIGHTWARDS BLACK ARROW} KM', bg='black',fg='white', command=mph_to_km)
+btn1 = Button(text='MPH \N{RIGHTWARDS BLACK ARROW} KM', bg='black',fg='white', command=mph_to_km, font='Tahoma')
 btn1.place(x=293,y=80)
-btn2 = Button(text='KM \N{RIGHTWARDS BLACK ARROW} MPH', bg='black',fg='white', command=km_to_mph)
+btn2 = Button(text='KM \N{RIGHTWARDS BLACK ARROW} MPH', bg='black',fg='white', command=km_to_mph, font='Tahoma')
 btn2.place(x=293, y=135)
 
 
 window.mainloop()
-
